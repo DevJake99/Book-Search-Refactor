aa8442fc43c782ded0c6b956e834c561571790cb
DevJake99 <jacobg4@uci.edu>
2023-12-13 02:12:36 -0800

    Created typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,54 @@
+const { gql } = require('apollo-server');
+
+const typeDefs = gql`
+type Book{
+    _id: ID!
+    authors: [String]
+    description: String
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+}
+type User{
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+    bookCount: Int
+    savedBooks: [Book]
+}
+type Auth{
+    token: ID!
+    user: User
+}
+
+type Query{
+    me: User
+}
+
+type Mutation{
+    createUser(
+        username:String!,
+        email: String!,
+        password: String!
+    ) : Auth
+    login(
+        email: String!,
+        password: String!
+    ): Auth
+
+    saveBook(
+        authors: [String],
+        description: String,
+        title: String,
+        bookId: String,
+        image: String,
+        link: String
+    ): User
+
+    deleteBook(bookId: string): User
+    
+}
+`;
+module.exports = typeDefs;
